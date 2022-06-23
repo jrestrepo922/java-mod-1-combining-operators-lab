30ae45126168f042120c63765b5cc6ec743620ec
Juan Martin Restrepo <tinto922@gmail.com>
2022-06-23 17:43:49 -0400

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,18 +6,20 @@ public class Main {
         try(Scanner scanner = new Scanner(System.in)){
             System.out.println("Type either && or ||");
             String input = scanner.nextLine();
+            boolean trueVar = true; 
+            boolean falseVar = false; 
             if(input.equals("&&")){
                 System.out.println("Truth table for && operator");
-                System.out.println("true && true = true");
-                System.out.println("true && false = false");
-                System.out.println("false && false = false");
-                System.out.println("false && true = false");
+                System.out.println("true && true = " + (trueVar && trueVar));
+                System.out.println("true && false = " + (trueVar && falseVar));
+                System.out.println("false && false = " + (falseVar && falseVar));
+                System.out.println("false && true = " + (falseVar && trueVar));
             } else if(input.equals("||")){
                 System.out.println("Truth table for || operator");
-                System.out.println("true || true = true");
-                System.out.println("true || false = true");
-                System.out.println("false || false = false");
-                System.out.println("false || true = true");
+                System.out.println("true || true = " + (trueVar || trueVar));
+                System.out.println("true || false = " + (trueVar || falseVar));
+                System.out.println("false || false = " + (falseVar || falseVar));
+                System.out.println("false || true = " + (falseVar || trueVar));
             } else {
                 System.out.println("please provide either a && or || to see the correct input");
             }
